8912065e686798c6f0bb5353cb2a894d591e8889
Karl Otness <karl.otness@nyu.edu>
2024-02-12 16:36:03 -0500

    Remove compile-time pyFFTW detection.

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-import warnings
 import numpy as np
 
-# This hack tells cython whether pyfftw is present
-use_pyfftw_file = 'pyqg/.compile_time_use_pyfftw.pxi'
-with open(use_pyfftw_file, 'wb') as f:
-    try:
-        import pyfftw
-        f.write(b'DEF PYQG_USE_PYFFTW = 1')
-    except ImportError:
-        f.write(b'DEF PYQG_USE_PYFFTW = 0')
-        warnings.warn('Could not import pyfftw. Model may be slower.')
 
 ext_module = Extension(
     "pyqg.kernel",
